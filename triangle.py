41b7c7a5110f8d36a0569afa1fc67ae395f8a7f6
SGiuri <s.giuri@issm.it>
2020-09-21 23:09:37 +0200

    Done Degenerate function

--- triangle.py
+++ triangle.py
@@ -1,10 +1,43 @@
 def equilateral(sides):
-    pass
+    if is_valid_triangle(sides):
+        return sides[0] == sides[1] == sides[2]
+    return False
 
 
 def isosceles(sides):
-    pass
+    if is_valid_triangle(sides):
+        return sides[0] == sides[1] or sides[1] == sides[2] or sides[2] == sides[0]
+    return False
 
 
 def scalene(sides):
-    pass
+    if is_valid_triangle(sides):
+        return not isosceles(sides)
+    return False
+
+
+def degenerate(sides):
+
+    if (sides[0] + sides[1] == sides[2]) or (
+            sides[1] + sides[2] == sides[0]) or (
+            sides[2] + sides[0] == sides[1]):
+        return True
+    return False
+
+
+def is_valid_triangle(sides):
+    if len(sides) != 3:
+        return False
+
+    for side in sides:
+        if side <= 0:
+            return False
+
+    if (sides[0] + sides[1] < sides[2]) or (
+            sides[1] + sides[2] < sides[0]) or (
+            sides[2] + sides[0] < sides[1]):
+        if degenerate(sides):
+            return True
+        return False
+
+    return True
